679989f128e7f7be3cc887864598ba1a40979798
Raghav S <prajapatpraveen722@gmail.com>
2024-04-15 19:05:56 +0530

    function name change

--- ios/DocumentViewController/RNTPTDocumentController.m
+++ ios/DocumentViewController/RNTPTDocumentController.m
@@ -110,6 +110,9 @@ NS_ASSUME_NONNULL_END
 
 - (void)toolManagerToolChanged:(PTToolManager *)toolManager
 {
+    [PTColorDefaults setDefaultColor:[UIColor greenColor] forAnnotType:PTExtendedAnnotTypeCircle attribute:ATTRIBUTE_STROKE_COLOR colorPostProcessMode:e_ptpostprocess_none];
+    [PTColorDefaults setDefaultColor:[UIColor greenColor] forAnnotType:PTExtendedAnnotTypeLine attribute:ATTRIBUTE_STROKE_COLOR colorPostProcessMode:e_ptpostprocess_none];
+    NSLog(@"toolManagerToolChanged3");
     PTTool *tool = toolManager.tool;
     
     const BOOL backToPan = tool.backToPanToolAfterUse;
@@ -142,9 +145,9 @@ NS_ASSUME_NONNULL_END
 {
     if ([toolManager.tool isKindOfClass:[PTAnnotEditTool class]]) {
            // Remove the annotation creation menu items.
-        [menuController setMenuItems:[self removeAnnotationItems:menuController.menuItems]];
-        [menuController setMenuItems:[self addAnnotationItems:menuController.menuItems withAccess:(PTAnnot *)annotation]];
-        [menuController setMenuItems:[self updateAnnotationItems:menuController.menuItems]];
+        [menuController setMenuItems:[self removeAnnotationMenuItems:menuController.menuItems]];
+        [menuController setMenuItems:[self addAnnotationMenuItems:menuController.menuItems withAccess:(PTAnnot *)annotation]];
+        [menuController setMenuItems:[self updateAnnotationMenuItems:menuController.menuItems]];
         NSLog(@"select annotation call4%@",menuController.menuItems);
         
            //return YES;
@@ -175,13 +178,13 @@ NS_ASSUME_NONNULL_END
     return showMenu;
 }
 
-- (NSArray<UIMenuItem *> *)updateAnnotationItems:(NSArray<UIMenuItem *> *)items {
+- (NSArray<UIMenuItem *> *)updateAnnotationMenuItems:(NSArray<UIMenuItem *> *)items {
     NSMutableArray<UIMenuItem *> *updatedItems = [items mutableCopy];
     
     // Define the title of the menu item you want to change.
     NSString *itemToChangeTitle = PTLocalizedString(@"Style", nil);
     // Define the new title for the menu item.
-    NSString *newItemTitle = @"Edit";
+    NSString *newItemTitle = @"Edit ";
     
     // Find the index of the menu item with the specified title.
     NSUInteger index = [updatedItems indexOfObjectPassingTest:^BOOL(UIMenuItem *menuItem, NSUInteger idx, BOOL *stop) {
@@ -199,7 +202,7 @@ NS_ASSUME_NONNULL_END
     return [updatedItems copy];
 }
 
-- (NSArray<UIMenuItem *> *)removeAnnotationItems:(NSArray<UIMenuItem *> *)items {
+- (NSArray<UIMenuItem *> *)removeAnnotationMenuItems:(NSArray<UIMenuItem *> *)items {
     NSLog(@"removeAnnotation call");
     NSArray<NSString *> *stringsToRemove = @[
         PTLocalizedString(@"Note", nil),
@@ -218,16 +221,20 @@ NS_ASSUME_NONNULL_END
 
 
 - (void) handleSetAnnotationAccess:(void(^)(void))handler {
-    PTAnnot* annotation = self.toolManager.tool.currentAnnotation;
-    NSString *annotationAccess = [annotation GetCustomData:@"access"];
-    NSLog(@"handle 1 %@", annotationAccess);
+//    PTAnnot* annotation = self.toolManager.tool.currentAnnotation;
+//    NSString *annotationAccess = [annotation GetCustomData:@"access"];
+//    NSLog(@"handle 1 %@", annotationAccess);
+//    if ([annotationAccess isEqualToString:@"private"]) {
+//        [annotation SetCustomData:@"access" value:@"public"];
+//    } else if ([annotationAccess isEqualToString:@"public"]) {
+//      [annotation SetCustomData:@"access" value:@"private"];
+//    }
+//    NSString *annotationAccesas = [annotation GetCustomData:@"access"];
+//    NSLog(@"handle 2 %@", annotationAccesas);
 }
 
-- (void) handleSetAnnotationAccess2:(void(^)(void))handler {
-    NSLog(@"handle 2");
-}
 
-- (NSArray<UIMenuItem *> *)addAnnotationItems:(NSArray<UIMenuItem *> *)items withAccess:(PTAnnot *)annotation {
+- (NSArray<UIMenuItem *> *)addAnnotationMenuItems:(NSArray<UIMenuItem *> *)items withAccess:(PTAnnot *)annotation {
     NSMutableArray<UIMenuItem *> *newItems = [items mutableCopy];
     NSString *annotationAccess = [annotation GetCustomData:@"access"];
     // Add new menu items to the popup based on the access.
@@ -235,7 +242,7 @@ NS_ASSUME_NONNULL_END
         UIMenuItem *publicMenuItem = [[UIMenuItem alloc] initWithTitle:@"Make Public" action:@selector(handleSetAnnotationAccess:)];
                 [newItems insertObject:publicMenuItem atIndex:1]; // Insert at index 1
     } else if ([annotationAccess isEqualToString:@"public"]) {
-        UIMenuItem *privateMenuItem = [[UIMenuItem alloc] initWithTitle:@"Set Private" action:@selector(handleSetAnnotationAccess2:)];
+        UIMenuItem *privateMenuItem = [[UIMenuItem alloc] initWithTitle:@"Set Private" action:@selector(handleSetAnnotationAccess:)];
                 [newItems insertObject:privateMenuItem atIndex:1]; // Insert at index 1
     }
     
